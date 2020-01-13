6d38b325dc3cd69c33985035043d2cf774d394b8
Wei <Wei>
2020-01-13 18:22:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="App">
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+body{
+  font-family: Arial, Helvetica, sans-serif, Microsoft YaHei;
+  position:relative;
+  display:block;
+  font-size:100%;
+  cursor:default;
+  margin:0;
+  padding:0;
+}
+a{
+  text-decoration:none;
+}
+#App{
+  position:relative;
+  display:block;
+  width:100%;
+  height:auto;
+  overflow:hidden;
+}
+</style>
+
+
---
+++ src/components/login/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- login_box -->
+    <div class="login_box">
+        <!-- login -->
+        <div class="login">
+            1234567
+        </div>
+        <!-- /login -->
+    </div>
+    <!-- /login_box -->
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.login_box{
+    position: relative;
+    display:block;
+    width:100%;
+    height:16em;
+    background: -webkit-linear-gradient(-90deg, #2e73ff , #184ebc);
+    background: -o-linear-gradient(-90deg, #2e73ff , #184ebc);
+    background: -moz-linear-gradient(-90deg, #2e73ff , #184ebc);
+    background: linear-gradient(-90deg, #2e73ff , #184ebc);
+}
+</style>
